dbba82a7f407f47aef7517ec7f4c6e5d64d6d61b
nitin12384 <nknitinkumar12384@gmail.com>
2022-09-10 13:08:08 +0530

    trying package importing in bot_runner

---
+++ bot_runner.py
@@ -0,0 +1,8 @@
+from botlib import Logger
+
+def main():
+    Logger.log("Bot Runner Initiated")
+
+if __name__ == "__main__":
+    main()
+
--- botlib/__init__.py
+++ botlib/__init__.py
@@ -1,2 +1,2 @@
 
-
+from .logging import (ConsoleLogger, Logger)

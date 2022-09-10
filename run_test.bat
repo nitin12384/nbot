8309118fd27e804749f497f424cedcedf752cdc2
nitin12384 <nknitinkumar12384@gmail.com>
2022-09-10 12:50:49 +0530

    edit batch files to run bot and tests

--- run_test.bat
+++ run_test.bat
@@ -2,7 +2,7 @@
 
 Rem if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
-start cmd /k python bot.py
+start cmd /k python ./testing/unit_test/test0.py
 
 
 
--- testing/unit_test/test0.py
+++ testing/unit_test/test0.py
@@ -3,7 +3,7 @@ import unittest
 class TestWorking(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(2+2, 4)
-        print("Test Passed.")
+        print("Test Passed. Unit Testing framework is working.")
 
 
 

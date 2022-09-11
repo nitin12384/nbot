611c393ff542c40b496a8fda84feda90d155d719
nitin12384 <nknitinkumar12384@gmail.com>
2022-09-11 14:30:27 +0530

    add file logging

--- botlib/logging.py
+++ botlib/logging.py
@@ -3,29 +3,42 @@ from enum import Enum
 
 import time
 
+_log_file_path = "./botFiles/log.txt"
+
+_log_into_file_default = True
+
 class LoggingLevel(Enum):
     INFO = 1
     LOG = 2
     WARN = 3
     ERROR = 4
 
-class ConsoleLogger:
+class Logger:
 
     def __init__(self, logging_level = LoggingLevel.LOG):
         self.logging_level = logging_level
+        self.log_file = open(_log_file_path, 'w')
 
-    def log(self, msg):
+    def _do_logging(self, msg, file_logging):
+        if file_logging:
+            self.log_file.write(Logger.add_time(msg))
+        else :
+            print(Logger.add_time(msg))
+
+    def log(self, msg, file_logging=_log_into_file_default):
         if self.logging_level.value <= LoggingLevel.LOG.value :
-            print(ConsoleLogger.add_time(msg))
+            self._do_logging("[LOG]"+msg, file_logging)
     
-    def info(self, msg):
+    def info(self, msg, file_logging=_log_into_file_default):
         if self.logging_level.value <= LoggingLevel.INFO.value :
-            print(ConsoleLogger.add_time(msg))
+            self._do_logging("[INFO]"+msg, file_logging)
+
+
 
     @staticmethod
     def add_time(msg):
         return "["+time.asctime()+"] : "+str(msg) 
-    
-Logger = ConsoleLogger()
+
+Logger = Logger()
 
 
